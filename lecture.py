d2b308a018441a95a6ad5166b74af4fc42edccb9
Doniyorbek Boltayev <96922916+doniyorbek-boltayev@users.noreply.github.com>
2025-05-18 18:02:31 +0100

    Update lecture.py

--- lecture.py
+++ lecture.py
@@ -1,11 +1,1 @@
-print("Hello " + input("What is your name?") + "!")
 
-name = input("What is your name?")
-print(len(name))
-
-username = input("Enter your name: ")
-length = len(username)
-print(length)
-
-# Learned built-in functions: input(), print(), len()
-# New concept: variables
