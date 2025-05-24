c32a1eff76c520f4880f2fd3fd4999244036a626
Doniyorbek Boltayev <96922916+doniyorbek-boltayev@users.noreply.github.com>
2025-05-24 04:15:04 +0100

    Create lecture.py

---
+++ day-6/lecture.py
@@ -0,0 +1,26 @@
+# This are answers to the Hurdle 1,2,3 and 4 on Reaborg's World
+# For more info follow this link: https://reeborg.ca/index_en.html
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right() == True:
+        move()
+        
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear() == True:
+        move()
+    else:
+        jump()
+        
